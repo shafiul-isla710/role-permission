30c7614f5c83cb5ab8bc7c7bb7c37c94482d8fa6
shafiul <shafiul8sms@gmail.com>
2025-05-13 21:41:16 +0600

    font end layouting with blade template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,7 +20,7 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-
-Route::get('/test',function(){
-   
+Route::middleware(['auth','verified'])->group(function(){
+   Route::get('user-logout',[UserController::class, 'logout'])->name('user.logout');
 });
+
